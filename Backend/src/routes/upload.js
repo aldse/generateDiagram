a1e5d001a6faa4b81ef59d1f5ae8b209ca110de3
Renato Mendes <rnt.mendes11@gmail.com>
2024-07-05 08:46:52 -0300

    Att

--- Backend/src/routes/upload.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UploadController = require('../controller/uploadController');
-
-router
-    .post('/upload', UploadController.uploadFile);
-
-module.exports = router;
