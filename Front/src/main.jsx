2ca5ade003d0322120be67f4ef2f46b532545231
Renato Mendes <rnt.mendes11@gmail.com>
2024-07-15 08:48:16 -0300

    Limpando projeto

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import  Home  from "./pages/Home";
 import  AddArq  from "./pages/AddArq";
-import  TestJ  from "./pages/Teste";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Cadastro from "./pages/Cadastro/index.jsx";
@@ -14,17 +12,14 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "/page1",
+    path: "/cadastro",
     element: <Cadastro />,
   },
   {
-    path: "/page2",
+    path: "/home",
     element: <AddArq />,
   },
-  {
-    path: "/test",
-    element: <TestJ />,
-  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
