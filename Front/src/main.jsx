aa58e17d4ceb0055cd3988357d5f4cd62d71947f
Renato Mendes <rnt.mendes11@gmail.com>
2024-07-15 11:09:06 -0300

    Proteção de rotas OK

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/authContext";
+import { Route } from "react-router-dom";
 import Login from "./pages/Login";
 import AddArq from "./pages/AddArq";
 import Cadastro from "./pages/Cadastro";
 import ProtectedRoute from "./components/ProtectedRoutes/protectedRoute";
 import "./index.css";
 
-const router = createBrowserRouter([
+const routes = [
   {
     path: "/",
     element: <Login />,
@@ -20,11 +21,17 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: <ProtectedRoute element={<AddArq />} />,
-  }
-]);
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}>
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+    </RouterProvider>
   </AuthProvider>
 );
